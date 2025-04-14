cf3614e10b96228f8f8f1ee5d31c9c627622edb2
Richixs <ricardoramirezcaero123@gmail.com>
2025-04-13 23:23:34 -0400

    clase Dessert

---
+++ app/src/main/java/cateringpostres/model/Dessert.java
@@ -0,0 +1,53 @@
+package cateringpostres.model;
+
+public class Dessert {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private byte[] image;
+
+    public Dessert(int id, String name, String description, double price, byte[] image) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+}
